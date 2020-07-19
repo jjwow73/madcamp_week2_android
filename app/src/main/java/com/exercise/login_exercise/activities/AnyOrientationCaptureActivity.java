fa5b5f92d2fc72c8a656571a67551fe25ea0ec3d
JongWoo Park <jjongwoo.dev@gmail.com>
2020-07-20 01:16:50 +0900

    feat: 세번째 탭, QR Scanner 구현
    Scanner로 인식 후 result String 반환(Toast)

---
+++ app/src/main/java/com/exercise/login_exercise/activities/AnyOrientationCaptureActivity.java
@@ -0,0 +1,13 @@
+package com.exercise.login_exercise.activities;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by jongwow on 2020-07-19.
+ */
+public class AnyOrientationCaptureActivity extends CaptureActivity {
+    /**
+     * This Activity is exactly the same as CaptureActivity, but has a different orientation
+     * setting in AndroidManifest.xml.
+     */
+}
---
+++ app/src/main/java/com/exercise/login_exercise/activities/CustomScannerActivity.java
@@ -0,0 +1,61 @@
+package com.exercise.login_exercise.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.exercise.login_exercise.BackPressHandler;
+import com.exercise.login_exercise.R;
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+/**
+ * Created by jongwow on 2020-07-19.
+ */
+public class CustomScannerActivity extends Activity {
+
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
+    private BackPressHandler backPressHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_scanner);
+
+        backPressHandler = new BackPressHandler(this);
+
+        barcodeScannerView = (DecoratedBarcodeView) findViewById(R.id.zxing_barcode_scanner);
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.decode();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        capture.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        capture.onSaveInstanceState(outState);
+    }
+
+    public void onBackPressed() {
+        backPressHandler.onBackPressed();
+    }
+
+}
